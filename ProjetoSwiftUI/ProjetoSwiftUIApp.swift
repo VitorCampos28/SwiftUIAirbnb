5d0681b02fdb6c85ec0953a4a0442f0b6af4b8a0
Vitor Campos <vitor.natal@timwe.com>
2023-10-30 10:31:36 -0300

    Initial Commit

---
+++ ProjetoSwiftUI/ProjetoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoSwiftUIApp.swift
+//  ProjetoSwiftUI
+//
+//  Created by Vitor Campos on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
